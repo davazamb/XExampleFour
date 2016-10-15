8f01f4d50ba90ad3e89cf89c6b30321d7df9e00a
David Zambrano <davazamb10@outlook.com>
2016-10-15 18:59:33 -0400

    WelcomePage

---
+++ XExampleFour/XExampleFour/Services/NavigationService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XExampleFour.Pages;
+
+namespace XExampleFour.Services
+{
+    public class NavigationService
+    {
+        public async void Navigate(string PageName)
+        {
+            App.Master.IsPresented = false;
+
+            switch (PageName)
+            {
+                case "AlarmsPage":
+                    await App.Navigator.PushAsync(new AlarmsPage());
+                    break;
+                case "ClientsPage":
+                    await App.Navigator.PushAsync(new ClientsPage());
+                    break;
+                case "SettingsPage":
+                    await App.Navigator.PushAsync(new SettingsPage());
+                    break;
+                case "NewOrderPage":
+                    await App.Navigator.PushAsync(new NewOrderPage());
+                    break;
+                case "MainPage":
+                    await App.Navigator.PopToRootAsync();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+}
---
+++ XExampleFour/XExampleFour/ViewModels/MainViewModel.cs
@@ -0,0 +1,105 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using XExampleFour.Pages;
+using XExampleFour.Services;
+
+namespace XExampleFour.ViewModels
+{
+    public class MainViewModel
+    {
+        #region Attributes
+        private NavigationService navigationService;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<MenuItemViewModel> Menu { get; set; }
+        public ObservableCollection<OrderViewModel> Orders { get; set; }
+        #endregion
+        #region Constructors
+        public MainViewModel()
+        {
+            navigationService = new NavigationService();
+            LoadMenu();
+            LoadData();
+        }
+        #endregion
+        #region Commands
+        public ICommand GoToCommand { get { return new RelayCommand<string>(GoTo); } }
+
+        private void GoTo(string pageName)
+        {
+            navigationService.Navigate(pageName);
+        }
+        public ICommand StartCommand { get { return new RelayCommand(Start); } }
+
+        private void Start()
+        {
+            navigationService.SetMainPage(new MasterPage());
+        }
+
+        #endregion
+
+        #region Methods
+        internal void SetMainPage(Page page)
+        {
+            App.Current.MainPage = page;
+        }
+
+        private void LoadData()
+        {
+            Orders = new ObservableCollection<OrderViewModel>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Orders.Add(new OrderViewModel
+                {
+                    Title = "Hola, Buen dia",
+                    DeliveryDate = DateTime.Today,
+                    Description = "Haciendo uso de xamarin app, TecnicomputoZ",
+                });
+            }
+        }
+
+
+        private void LoadMenu()
+        {
+            Menu = new ObservableCollection<MenuItemViewModel>();
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_orders.png",
+                PageName = "MainPage",
+                Title = "Pedidos",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_client.png",
+                PageName = "ClientsPage",
+                Title = "Clientes",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_alarm.png",
+                PageName = "AlarmsPage",
+                Title = "Alarmas",
+            });
+
+            Menu.Add(new MenuItemViewModel
+            {
+                Icon = "ic_action_settings.png",
+                PageName = "SettingsPage",
+                Title = "Ajustes",
+            });
+        }
+        #endregion
+    }
+
+}
